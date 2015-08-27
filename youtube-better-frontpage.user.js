d31ac7422a237a6648dd2f281947a230d4bfb49e
Poorchop <poorchop@users.noreply.github.com>
2015-08-26 23:51:29 -0400

    Update youtube-better-frontpage.user.js
    -Rewrite in preparation for using MutationObserver

--- youtube-better-frontpage.user.js
+++ youtube-better-frontpage.user.js
@@ -3,63 +3,29 @@
 // @namespace   https://github.com/Poorchop/userscripts
 // @description Hides the nasty bits on the default Youtube front page, leaving only the important bits
 // @include     /https?:\/\/www\.youtube\.com\/$/
-// @version     0.2
+// @version     1.0
 // @grant       none
 // ==/UserScript==
 
 /* jshint browser: true, esnext: true */
 
-function removeGarbage() {
-  let body = document.getElementById("body");
+function addCustomCSS() {
+  let betterCSS = "#body-container { background: #fff !important; }" +
+      "#yt-masthead-container { background: #fff !important; border-bottom: none !important; }" +
+      ".yt-masthead-logo-container { float: none !important; margin-top: 250px !important;" +
+      "position: unset !important; text-align: center !important; width: unset !important; }" +
+      "#yt-masthead-signin { float: none !important; margin-top: unset !important; margin-left: unset !important;" +
+      "position: absolute !important; right: 10px !important; top: 10px !important; }" +
+      "#masthead-search { margin: auto !important; }" +
+      "#appbar-guide-button-container { display: none !important; }" +
+      "#page-container { display: none !important; }" +
+      "#footer-container {display: none !important; }";
 
-  // remove the footer
-  let footer = document.getElementById("footer-container");
-  body.removeChild(footer);
-
-  // unclutter the main container
-  let bodyContainer = document.getElementById("body-container");
-  bodyContainer.removeChild(document.getElementById("a11y-announcements-container"));
-  bodyContainer.removeChild(document.getElementById("masthead-positioner-height-offset"));
-  bodyContainer.removeChild(document.getElementById("page-container"));
-
-  // clean up the header and apply/remove styles
-  bodyContainer.style.background = "#fff";
-
-  let mastheadContainer = document.getElementById("yt-masthead-container");
-  mastheadContainer.removeChild(document.getElementById("a11y-skip-nav"));
-  mastheadContainer.style.background = "#fff";
-  mastheadContainer.style.borderBottom = "none";
-
-  let mastheadLogoContainer = document.getElementsByClassName("yt-masthead-logo-container")[0];
-  mastheadLogoContainer.removeChild(document.getElementById("appbar-guide-button-container"));
-  mastheadLogoContainer.style.marginTop = "250px";
-  mastheadLogoContainer.style.textAlign = "center";
-  mastheadLogoContainer.style.float = "none";
-  mastheadLogoContainer.style.position = "unset";
-  mastheadLogoContainer.style.width = "unset";
-
-  let mastheadSignin = document.getElementById("yt-masthead-signin");
-  if (!mastheadSignin) {
-    // case for when user is logged in
-    // reuse mastheadSignin assignment to more easily find YouTube Center button
-    mastheadSignin = document.getElementById("yt-masthead-user");
-  }
-  // TODO: nicely display subscriptions when user is logged in - delete the node for now
-  document.getElementById("masthead-positioner").removeChild(document.getElementById("masthead-appbar-container"));
-  mastheadSignin.style.float = "none";
-  mastheadSignin.style.marginLeft = "unset";
-  mastheadSignin.style.marginTop = "unset";
-  mastheadSignin.style.position = "absolute";
-  mastheadSignin.style.right = "10px";
-  mastheadSignin.style.top = "10px";
-
-  document.getElementById("masthead-search").style.margin = "auto";
-
-  // set style for YouTube Center button
-  let ytCenterBtn = mastheadSignin.children[mastheadSignin.childElementCount - 1];
-  if (ytCenterBtn.getAttribute("title") === "Toggle YouTube Center Settings panel") {
-    ytCenterBtn.style.margin = "0px 0px 0px 10px";
-  }
+  let betterStyle = document.createElement("style");
+  betterStyle.type = "text/css";
+  betterStyle.id = "better-css";
+  betterStyle.appendChild(document.createTextNode(betterCSS));
+  document.head.appendChild(betterStyle);
 }
 
-removeGarbage();
+addCustomCSS();
