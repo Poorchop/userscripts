0cdca6d974e207d22a0c2339ec1fc644ca4fc211
Poorchop <poorchop@users.noreply.github.com>
2015-08-27 17:30:39 -0400

    Update youtube-better-frontpage.user.js
    -Code style tweaks
    -Adjust style when user is logged in
    -Style YouTube Center button

--- youtube-better-frontpage.user.js
+++ youtube-better-frontpage.user.js
@@ -2,8 +2,8 @@
 // @name        Youtube Better Front Page
 // @namespace   https://github.com/Poorchop/userscripts
 // @description Hides the nasty bits on the default Youtube front page, leaving only the important bits
-// @include     /https?:\/\/www\.youtube\.com\/$/
-// @version     1.0
+// @include     http*://www.youtube.com/*
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
@@ -11,55 +11,19 @@
 
 let observer;
 
-init();
-
-function init() {
-  /* don't do anything inside iframes */
-  if (window.self !== window.top) {
-    return;
-  }
-
-  observer = new MutationObserver(body_change);
-  observer.observe(document.body, { childList: true });
-
-  apply_styles();
-}
-
-function body_change(mutations) {
-  for (let mutation of mutations) {
-    for (let element of mutation.addedNodes) {
-      if (element.id == 'progress') {
-        apply_styles();
-      }
-    }
-  }
-}
-
-function apply_styles() {
-  if (window.location.pathname == '/') {
-    addCustomCSS();
-  }
-  else {
-    let styles = document.getElementById('better-css');
-
-    if (styles) {
-      styles.parentElement.removeChild(styles);
-    }
-  }
-}
-
 function addCustomCSS() {
   let betterCSS = "#body-container { background: #fff !important; }" +
       "#yt-masthead-container { background: #fff !important; border-bottom: none !important; }" +
       ".yt-masthead-logo-container { float: none !important; margin-top: 250px !important;" +
       "position: unset !important; text-align: center !important; width: unset !important; }" +
-      "#yt-masthead-signin { float: none !important; margin-top: unset !important; margin-left: unset !important;" +
-      "position: absolute !important; right: 10px !important; top: 10px !important; }" +
+      "#yt-masthead-signin, #yt-masthead-user { float: none !important; margin-top: unset !important;" +
+      "margin-left: unset !important; position: absolute !important; right: 10px !important; top: 10px !important; }" +
       "#masthead-search { margin: auto !important; }" +
       "#appbar-guide-button-container { display: none !important; }" +
       "#page-container { display: none !important; }" +
       "#footer-container {display: none !important; }" +
-      "#masthead-appbar { display: none; }";
+      "#masthead-appbar { display: none; }" +
+      "#masthead-user-button { margin: 0px 0px 0px 10px}"; // YouTube Center button
 
   let betterStyle = document.createElement("style");
   betterStyle.type = "text/css";
@@ -68,3 +32,38 @@ function addCustomCSS() {
   document.head.appendChild(betterStyle);
 }
 
+function applyStyles() {
+  if (window.location.pathname == "/") {
+    addCustomCSS();
+  } else {
+    let styles = document.getElementById("better-css");
+
+    if (styles) {
+      styles.parentElement.removeChild(styles);
+    }
+  }
+}
+
+function bodyChange(mutations) {
+  for (let mutation of mutations) {
+    for (let element of mutation.addedNodes) {
+      if (element.id === "progress") {
+        applyStyles();
+      }
+    }
+  }
+}
+
+function init() {
+  /* don't do anything inside iframes */
+  if (window.self !== window.top) {
+    return;
+  }
+
+  observer = new MutationObserver(bodyChange);
+  observer.observe(document.body, { childList: true });
+
+  applyStyles();
+}
+
+init();
